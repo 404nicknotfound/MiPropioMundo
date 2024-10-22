8ae3a600f289fff33092a30687402bb7e3563f90
404NickNotFound <2paulo2gaming2@gmail.com>
2024-10-21 22:11:11 -0500

    Calc

---
+++ Calculadora/operaciones.py
@@ -0,0 +1,23 @@
+
+def suma(valor1,valor2):
+    resultado = valor1 + valor2
+    return resultado
+
+def resta(valor1,valor2):
+    resultado = valor1 - valor2
+    return resultado
+
+def multi(valor1,valor2):
+    resultado = valor1 * valor2
+    return resultado
+
+def division(valor1,valor2):
+    resultado = valor1 / valor2
+    return resultado
+
+def potencia(valor1,valor2,):
+    resultado = 1
+    for i in range(1,(valor2+1)):
+        resultado*=valor1
+    return resultado
+
